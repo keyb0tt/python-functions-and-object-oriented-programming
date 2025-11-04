f79d5105af545d6f805c625be7fc955a3b274c58
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-11-04 10:21:20 -0300

    .

--- bloco_1/eh_par.py
+++ bloco_1/eh_par.py
@@ -1,1 +1,11 @@
 # Exercício 3: Faça uma função eh_par(numero) que retorna True se o número for par e False caso contrário.
+import os
+os.system('clear')
+
+def eh_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+    
+print(eh_par(12))
