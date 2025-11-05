1e693acb0b96cc2a9c27041e2cea60d5f623421f
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-11-05 09:41:25 -0300

    .

--- bloco_3/potencia.py
+++ bloco_3/potencia.py
@@ -1,3 +1,14 @@
 # Exercício 11: Crie potencia(base, expoente=2) onde o expoente padrão é 2.
 import os
-os.sytem('clear')
+os.system('clear')
+
+def potencia(base, expoente=2):
+    result = base
+
+    for i in range(1, expoente):
+        result *= base
+
+    return result
+
+potencia_var = potencia(15)
+potencia_var = potencia(4, 3)
